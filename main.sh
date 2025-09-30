c496f3fd5452e83fca903b4484a3a2adc250674a
Allan <53341442+Guohanzhong@users.noreply.github.com>
2025-09-30 23:10:45 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
 
 python main.py \
-  --llm_model_path "vlm/checkpoint-12844" \
+  --llm_model_path "checkpoints/vlm/checkpoint-12844" \
   --codec_config_path "configs/config_vq1024_resres_aug_decay0.99_q5_gcd_nl8_ld512.yaml" \
   --rt_config_path "configs/config_rt_euler.yaml" \
-  --image_path "/data/guohanzhong/data/collected_fashion_data_clean/img/rand_YHLMQOMVPI_render_front_cutted.png" \
+  --image_path "rand_YHLMQOMVPI_render_front_cutted.png" \
   --output_path "./my_garment.json" \
   --device "cuda:0"
